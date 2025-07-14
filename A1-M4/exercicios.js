299f92a22bc4552cf59c60ec2b88c85a8a89fc81
Tabs Macedo <tabata.macedo1897@gmail.com>
2025-07-14 18:54:35 -0300

    feat: adição exercicio aula 2

--- A1-M4/exercicios.js
+++ A1-M4/exercicios.js
@@ -1,79 +1,5 @@
-// promises para verificar idade
-
-function verificarIdade(idade) {
-  return new Promise((resolve, reject) => {
-    if (idade >= 18) {
-      resolve("🔓 Acesso liberado!");
-    } else {
-      reject("🔒 Acesso negado. Menor de idade.");
-    }
-  });
-}
-
-// Teste
-verificarIdade(15)
-  .then(console.log)
-  .catch(console.error);
 
 
-let falaPromessa = "vou te comprar um carro de verdade!"
-let falaFinal = "vou te comprar um carro de verdade!" // ou troque para "um carrinho"
-
-// função chamada executor (é uma função que recebe 2 parâmetros: resolve(value) o resolve é o que eu quero capturar no then), e o reject (reason) você chama quando dá errado, e ele vai diretamente no reason
-
-// Toda promise precisa de uma função que resolve (quando dá certo) ou rejeita (quando dá errado)
-
-new Promise(()=>{
-
-})
-
-// mas como eu chamo a promise ? eu chamo o nome da constante e os estados que ela pode executar, imagine que eu pedi comida por uma app (promise), se eu não abrir a porta para receber ela pode até chegar, mas você nunca vai ver ou usar o que veio
-// o then é para abrir a porta quando chega, e o catch é para ver o que ocorreu de problema e reagir a isso
-const promessaMae = new Promise(()=>{})
-
-// 🧠 Pergunta:
-// Por que eu uso só console.log direto no .then() e não uma arrow function como resposta => console.log(resposta)?
-
-// ✅ Porque console.log já é uma função.
-// Quando você faz:
-
-// javascript
-// Copiar
-// Editar
-// .then(console.log)
-// Você está passando a função console.log diretamente como o que deve ser executado quando a Promise for resolvida.
-
-// Ou seja, é o mesmo que isso:
-
-// javascript
-// Copiar
-// Editar
-// .then((resposta) => {
-//   console.log(resposta)
-// })
-// 🤯 Isso funciona porque...
-// O .then() espera uma função como argumento.
-// E console.log é uma função.
-
-// Então o JavaScript entende:
-
-// “Ah, quando o .then() for chamado, use console.log e passe o valor como parâmetro”.
-
-// 🧪 Exemplo mais claro:
-// Essas duas formas fazem a mesma coisa:
-
-// Forma 1 (com arrow function):
-// js
-// Copiar
-// Editar
-// .then(valor => console.log(valor))
-// Forma 2 (função direta):
-// js
-// Copiar
-// Editar
-// .then(console.log)
-
-// criando código para servidor
 
 const Voyager = {
     nome: "Voyager",
@@ -131,3 +57,23 @@ enviarDadosParaNasa(Juno)
   .catch(erro => console.log(erro));
 
 
+
+
+let idadeInformada = 16;
+let idadeEsperada = 18;
+
+const verificarIdade = new Promise((resolve, reject) => {
+  if (idadeInformada >= idadeEsperada) {
+    resolve("Maior de idade");
+  } else {
+    reject("Menor de idade");
+  }
+});
+
+verificarIdade
+  .then((mensagem) => {
+    console.log(mensagem);
+  })
+  .catch((erro) => {
+    console.log(erro);
+  });
