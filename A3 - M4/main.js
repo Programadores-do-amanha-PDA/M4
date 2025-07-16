97adfefbd6c3d3551fd6d8025cd75a2c5b08dff6
Tabs Macedo <tabata.macedo1897@gmail.com>
2025-07-16 19:24:26 -0300

    feat: adição da aula 4

--- A3 - M4/main.js
+++ A3 - M4/main.js
@@ -1,38 +1,41 @@
 const divResposta = document.getElementById('pokemon');
 
-async function buscarPokemon(nomeDigitado){
+async function buscarPokemon(nomeDigitado) {
 
-  try{
+  try {
     const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nomeDigitado}`);
 
-    if(!resposta.ok){
-      alert('Requisição falha')
+    if (!resposta.ok) {
+      throw new Error('Pokémon não encontrado');
     }
 
     const dados = await resposta.json();
-    console.log(dados)
 
-    divResposta.innerHTML = 
-    ` <h2> ${dados.name}</h2>
-      <img src="${dados.sprites.front_default}" />  
-    `
+    console.log(dados.sprites.front_default);
+
+    divResposta.innerHTML = `
+      <h2>${dados.name}</h2>
+      <img src="${dados.sprites.front_default}"/>
+      <p>Peso: ${dados.weight}</p>
+    `;
   }
-  catch(error){
-    alert(error)
+
+
+  catch (error) {
+    alert(error.message);
   }
-  
 }
 
 const buttonBuscar = document.getElementById('btn-buscar');
 const pokemonsPermitidos = ['ditto', 'charmander', 'mewto', 'lapras'];
 
 
-buttonBuscar.addEventListener('click', function(){
+buttonBuscar.addEventListener('click', function () {
   const nomeDigitado = document.getElementById('input-pokemon').value.toLowerCase();
 
-  if(pokemonsPermitidos.includes(nomeDigitado)){
+  if (pokemonsPermitidos.includes(nomeDigitado)) {
     buscarPokemon(nomeDigitado);
-  }else{
+  } else {
     divResposta.innerText = `Só é possível buscar por: Ditto, Charmander, Mewtwo ou Lapras`
   }
 
